f23aa06b2e4923a4a611b99064bb1f474d4a33ff
Wadhavane Dhananjay Laxman <56212893+dhananjay1999@users.noreply.github.com>
2021-04-01 17:34:14 +0530

    Add files via upload

---
+++ arrray_with_min_index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	short age[4];
+	
+	age[0] = 22;
+	age[1] = 30;
+	age[2] = 47;
+	age[3] = 12;
+	
+	for(int i=0; i<=7; ++i){
+		cout<<age[i]<< " ";
+	}
+	
+	
+	return 0;
+}
